d6694cb9d9ad4ffebbcae55e3900ddf6f6f9aaa2
lophome23 <prosperlozada@gmail.com>
2017-02-22 15:14:38 +0800

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Activity 9 </title>
+<script language ="Javascript" type= "text/javascript">
+<!--
+ 	function getValues(){
+ 		var retVal = prompt("Enter your name:","your name here");
+ 		document.write("Hi! "+ retVal);
+ 	}
+ //-->
+</script>
+</head>
+<body>
+<p> Click the following button to see the results</p>
+<form>
+<input type="button" value="ClickMe" onclick="getValues()"/>
+</form>
+</body>
+</html>
+
+
+
